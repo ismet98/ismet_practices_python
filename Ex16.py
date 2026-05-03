2523e6a2d0e16d179a505c17e78559818e2c95db
ozer <iozer@ust.hk>
2026-05-04 00:14:24 +0800

    new 16

--- Ex16.py
+++ Ex16.py
@@ -1,5 +1,5 @@
 
-
+## MUst Bew Uopdated
 
 ## Passwords Shoudld be Random, Generate a New Password
 
@@ -26,25 +26,36 @@ c = range(0,10)
 from string import punctuation
 d = list(punctuation)
 
+
+## this will create equal number of each character
 def random_password_generator(number):
+    
     password = ""
-    
-
     for item in range(number):
-        temp =[]
-        temp.append(random.choice(a))
-        temp.append(random.choice(b))
-        temp.append(random.choice(c))
-        temp.append(random.choice(d))
-        password = password + str((random.choice(temp)))
+        if item % 4 == 0:
+            password = password + str((random.choice(a)))
+        elif item % 4 == 1:
+            password = password + str((random.choice(b)))
+        elif item % 4 == 2:
+            password = password + str((random.choice(c)))
+        elif item % 4 == 3:
+            password = password + str((random.choice(d)))
     
+    ##random.shuffle(password)
+
+    password_list = list(password)
+    random.shuffle(password_list)
+    password = "".join(password_list)
+
     return password
-
-
+    
 
 
 number = int(input("How many characters long do you want your password to be: "))
-print(random_password_generator(number))
+
+password = random_password_generator(number)
+
+print(password)
 
 
 
