0e70a6592b6b5f84568ba69448ce0c5c6a33fe8d
ozer <iozer@ust.hk>
2026-05-07 22:20:29 +0800

    first upload

---
+++ CS61/lab00/lab00.py
@@ -0,0 +1,17 @@
+def twenty_twenty_six():
+    """Come up with the most creative expression that evaluates to 2026
+    using only numbers and the +, *, and - operators (or ** and % if you'd like).
+
+    >>> twenty_twenty_six()
+    2026
+    """
+
+    print(2026)
+    print
+
+
+
+    return 
+
+
+twenty_twenty_six()
